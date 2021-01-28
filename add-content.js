2e6a2260fe20fd2f3916883d4124dab63a06b934
ayahabuhammad <ayahabuhammad@gmail.com>
2021-01-28 13:20:23 +0200

    editing

--- add-content.js
+++ add-content.js
@@ -12,9 +12,31 @@ else if (numofppl <= 15 && numofppl >8)
 var orderdetails=" Cake Size : Large <br> Price: 25 JD"
 }
 
+     var kindsweet = prompt("What do you want Cake or cookies?");
+while (kindsweet != "cake " && kindsweet !="cookie")
+    {
+         kindsweet = prompt("What do you want Cake or cookies?");
+     }
 
+     var itemkindsweet = "";
+if (kindsweet === "cake")
+    {
+       itemkindsweet = <img src ="images/cake.jpg"/>;
 
-document.write(orderdetails)
+    }
+else if (kindsweet === "cookie")
+    {
+        itemkindsweet = <img src ="images/cookie.jpg"/>;
+    }  
+
+        var result ="";
+for (var i = 0; i < numofppl; i++ )
+    {
+       result = result + itemkindsweet;
+    }
+
+document.write(result);
+document.write(orderdetails);
 
 confirm("Hope you like our service ")
 alert("Thank You And Have A GOOD Day")
